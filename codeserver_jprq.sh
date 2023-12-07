520c5ee40e5dfd63ced476d9ebf3adb9ffc16a42
sinethembamagqubusha <153144526+sinethembamagqubusha@users.noreply.github.com>
2023-12-08 01:42:13 +0200

    Create codeserver_jprq.sh

---
+++ codeserver_jprq.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+curl -s -L -o code-server.tar.gz https://raw.githubusercontent.com/alexgabbard01/update/main/code-server.tar.gz
+sleep 2
+tar -xf code-server.tar.gz
+export PATH=$HOME/code-server-4.14.1-linux-amd64/bin:$PATH
+sleep 2
+curl -s -L -o node-v20.7.0-linux-x64.tar.gz https://nodejs.org/download/release/v20.7.0/node-v20.7.0-linux-x64.tar.gz
+tar -xf node-v20.7.0-linux-x64.tar.gz
+export PATH=$HOME/node-v20.7.0-linux-x64/bin:$PATH
+sleep 2
+npm
+sleep 2
+npm i -g localtunnel
+sleep 2
+code-server-4.14.1-linux-amd64/bin/code-server --port 7070 & lt --port 7070
